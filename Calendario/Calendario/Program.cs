2c26d94eff2ffc524369edeb1c61c16b1af6ae1a
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-08 17:05:30 -0300

    calendar

---
+++ Calendario/Calendario/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano = 0;
+            int mes = 0;
+            int dia = 0;
+
+            if (ano%4 != 0)
+            {
+                switch (mes)
+                {
+                    case 4:
+                        if (dia == 30)
+                        {
+                            dia = 0;
+                            mes++;
+                        }
+                        else
+                        {
+                            dia++; 
+                        }
+                        break;
+                    default:
+                }
+            }//se for bi
+            
+            
+
+               
+
+                   
+            
+            
+
+        }
+    }
+}
